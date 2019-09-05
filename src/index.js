51e79f933073377735c6e8d71f6bc6be0b525c9a
Walsker <wal.gatlei@gmail.com>
2019-09-05 01:17:36 -0400

    Add content

--- src/index.js
+++ src/index.js
@@ -2,10 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+import Logo from './assets/logo.png';
+
+const Button = ({label}) => (
+  <div className='buttonContainer'>
+    {label}
+  </div>
+);
+
 function App() {
   return (
     <>
-      
+      <img id='logo' src={Logo} alt='Saving Cindi Logo'/>
+      <h1>SAVING CINDI</h1>
+      <h4>NEXT SHOW</h4>
+      <p>Rooster's Coffeehouse, Carleton University<br/>September 12th</p>
+      <Button label='LISTEN'/>
     </>
   );
 }
