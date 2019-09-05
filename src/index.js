8cce34f398294a8d11700d6a5e54694f5bc8cbdf
Walsker <wal.gatlei@gmail.com>
2019-09-05 01:35:56 -0400

    Add social links

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { FontAwesomeIcon as Icon } from '@fortawesome/react-fontawesome'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons'
 import './index.css';
 
 import Logo from './assets/logo.png';
 
-const Button = ({label}) => (
-  <div className='buttonContainer'>
+const Button = ({label, link}) => (
+  <a href={link} className='buttonContainer'>
     {label}
-  </div>
+  </a>
+);
+
+const SocialLink = ({faIcon, label, link}) => (
+  <a href={link} className='socialButton'>
+    <Icon icon={faIcon} size='lg'/>
+    {label}
+  </a>
 );
 
 function App() {
@@ -18,6 +28,11 @@ function App() {
       <h4>NEXT SHOW</h4>
       <p>Rooster's Coffeehouse, Carleton University<br/>September 12th</p>
       <Button label='LISTEN'/>
+      <div id='socialLinks'>
+        <SocialLink faIcon={faTwitter} label='@SavingCindi' link=''/>
+        <SocialLink faIcon={faEnvelope} label='booking@savingcindi.ca' link=''/>
+        <SocialLink faIcon={faInstagram} label='@savingcindi' link=''/>
+      </div>
     </>
   );
 }
