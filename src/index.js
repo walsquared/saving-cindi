1fc02dec23f535019bc7b933e4cafd11717d77c4
walsker <walskerw@gmail.com>
2019-09-11 22:45:33 -0400

    Add paper bkg

--- src/index.js
+++ src/index.js
@@ -5,34 +5,31 @@ import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import { faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons'
 import './index.css';
 
-import Logo from './assets/logo.png';
+// import Logo from './assets/logo.png';
 
-const Button = ({label, link}) => (
-  <a href={link} className='buttonContainer'>
-    {label}
-  </a>
-);
+// const Button = ({label, link}) => (
+//   <a href={link} className='buttonContainer' target='_blank' rel='noopener noreferrer external'>
+//     {label}
+//   </a>
+// );
 
 const SocialLink = ({faIcon, label, link}) => (
-  <a href={link} className='socialButton'>
+  <a href={link} className='socialButton' target='_blank' rel='noopener noreferrer external'>
     <Icon icon={faIcon} size='lg'/>
-    {label}
   </a>
 );
 
 function App() {
   return (
     <>
-      <img id='logo' src={Logo} alt='Saving Cindi Logo'/>
-      <h1>SAVING CINDI</h1>
-      <h4>NEXT SHOW</h4>
-      <p>Rooster's Coffeehouse, Carleton University<br/>September 12th</p>
-      <Button label='LISTEN'/>
       <div id='socialLinks'>
-        <SocialLink faIcon={faTwitter} label='@SavingCindi' link=''/>
-        <SocialLink faIcon={faEnvelope} label='booking@savingcindi.ca' link=''/>
-        <SocialLink faIcon={faInstagram} label='@savingcindi' link=''/>
+        <SocialLink faIcon={faTwitter} label='SavingCindi' link='https://twitter.com/savingcindi'/>
+        <SocialLink faIcon={faEnvelope} label='booking@savingcindi.com' link='mailto:info@savingcindi.com'/>
+        <SocialLink faIcon={faInstagram} label='savingcindi' link='https://www.instagram.com/savingcindi/'/>
       </div>
+      {/* <img id='logo' src={Logo} alt='Saving Cindi Logo'/>
+      <h1>SAVING CINDI</h1>
+      <Button label='LISTEN' link='https://songwhip.com/album/the-experience/love-me'/> */}
     </>
   );
 }
